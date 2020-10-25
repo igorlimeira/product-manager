d17e723376a6a19f850040e2e6842e20663831fb
Bruno Henrique <azurekfl@gmail.com>
2020-10-25 20:27:20 -0300

    teste bruno

--- demo/src/main/java/com/example/demo/interfaces/UserInterface.java
+++ demo/src/main/java/com/example/demo/interfaces/UserInterface.java
@@ -9,3 +9,6 @@ import com.example.demo.entity.Person;
 public interface UserInterface extends JpaRepository<Person, Long>{
 	Optional <Person> findFirstByNome(String name);
 }
+
+System.out.print ('bom dia')
+
